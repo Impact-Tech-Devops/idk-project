0570def5f2234a8f54a00b4e3577807ed2b7eab0
unknown <confidenceefem1@gmail.com>
2024-04-05 21:41:34 +0100

    fix

---
+++ app/components/About.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+    return (
+        <div className="w-full flex justify-center py-6">
+            <div className="w-[80%] flex flex-col gap-[20px]  max-[500px]:w-[90%]">
+                <div className="font-bold text-[30px] max-[500px]:text-[25px]">About Us</div>
+                <div className="text-[rgb(74, 78, 86)] text-[16px] leading-[26px] max-[500px]:text-[14px] max-[500px]:24px ">
+                FITC is a world-class, innovation-led, and technology-driven knowledge organization, established in 1981 as a non-profit organisation limited by guarantee, based on the Company’s Act of 1968, in response to the recommendations of the Pius Okigbo Committee. The Committee was set up by the Federal Government of Nigeria to review the Nigerian financial system. FITC was established to provide innovative knowledge solutions and capacity building programmes that develop and strengthens resources for the Nigerian financial services sector through Advisory services, Learning, Research and Advocacy.
+                <br/>
+                <br/>
+FITC is owned by the Banker's Committee, which comprises of the Central Bank of Nigeria (CBN), the Nigeria Deposit Insurance Corporation (NDIC), all licensed banks, and discount houses in Nigeria.
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
