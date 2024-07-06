b96ca8fa1b05fecd863b70c2275cb1c5d9f5a322
Emmanuel Akinyemi Oluwadamilare <emmanuelakinyemi772@gmail.com>
2024-07-06 14:00:28 +0100

    all set...

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -1,161 +1,77 @@
 "use client";
 
+import { AcademicCapIcon, DesktopComputerIcon, DocumentTextIcon, TrendingUpIcon } from "@heroicons/react/outline"
+
 const Features = () => {
   return (
-    <div>
-      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto bg-gray-100/70">
+    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div className="grid md:grid-cols-2 gap-12">
+        <div className="lg:w-3/4 align-middle lg:pt-24 md:pt-20">
+          <h2 className="text-3xl text-gray-800 font-bold lg:text-4xl">
+            Our Services
+          </h2>
+          <p className="mt-3 text-gray-800">
+            We provide a wide range of services to help businesses and individuals achieve their goals.
+          </p>
+          <p className="mt-5">
+            <a className="inline-flex items-center gap-x-1 font-medium text-red-600" href="/services">
+              Explore our services
+              <svg className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg>
+            </a>
+          </p>
+        </div>
 
-        <div className="min-h-screen">
-          <div className="max-w-7xl mx-auto h-max px-6 md:px-12 xl:px-6">
-            <div className="md:w-2/3 lg:w-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6 text-gray-900"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <h2 className="my-8 text-2xl font-bold text-black md:text-4xl">
-                How we work?
-              </h2>
-              <p className="text-gray-600">
-                We follow our process to get you started as quickly as possible
+        <div className="space-y-6 lg:space-y-10">
+          <div className="flex">
+            <span className="flex-shrink-0 inline-flex justify-center items-center size-[46px] rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm mx-auto">
+              <AcademicCapIcon className="flex-shrink-0 size-5" />
+            </span>
+            <div className="ms-5 sm:ms-8">
+              <h3 className="text-base sm:text-lg font-semibold text-gray-800">
+                Training and Development
+              </h3>
+              <p className="mt-1 text-gray-600">
+                We offer capacity building, entrepreneurship training, and innovative business development programs.
               </p>
             </div>
-            <div className="mt-16 grid divide-x divide-y  divide-gray-700 overflow-hidden rounded-3xl border text-gray-600 border-gray-700 sm:grid-cols-2 lg:grid-cols-4  lg:divide-y-0 xl:grid-cols-4">
-              <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-                <div className="relative space-y-8 py-12 p-8">
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    color="white"
-                    style={{ color: 'rgba(255, 0, 0, 0.95)' }}
-                    height="50"
-                    width="50"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"></path>
-                    <path d="M10 10l2 -2v8"></path>
-                  </svg>
-                  <div className="space-y-2">
-                    <h5 className="text-xl font-semibold text-white transition">
-                      IDK Learning Centre Facilities.
-                    </h5>
-                    <p className="text-gray-300">
-                      We will have discussions on the requirements to ensure
-                      your MVP (Minimum Viable Product) is ready for the initial
-                      launch
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-                <div className="relative space-y-8 py-12 p-8">
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    color="white"
-                    style={{ color: 'rgba(255, 0, 0, 0.95)' }}
-                    height="50"
-                    width="50"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"></path>
-                    <path d="M10 8h3a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 0 -1 1v2a1 1 0 0 0 1 1h3"></path>
-                  </svg>
-                  <div className="space-y-2">
-                    <h5 className="text-xl font-semibold text-white transition group-hover:text-secondary">
-                      Skills Training and Entrepreneurship Development.
-
-                    </h5>
-                    <p className="text-gray-300">
-                      Once we agree on what to build, We will start working on
-                      it right away.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-                <div className="relative space-y-8 py-12 p-8">
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    color="white"
-                    style={{ color: 'rgba(255, 0, 0, 0.95)' }}
-                    height="50"
-                    width="50"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"></path>
-                    <path d="M10 9a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1"></path>
-                  </svg>
-                  <div className="space-y-2">
-                    <h5 className="text-xl font-semibold text-white transition group-hover:text-secondary">
-                      Teacher and Schools Development.
-
-                    </h5>
-                    <p className="text-gray-300">
-                      We will develop your product MVP in 15 days (more time
-                      required depending on the complexity of the project)
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-                <div className="relative space-y-8 py-12 p-8">
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    color="white"
-                    style={{ color: 'rgba(255, 0, 0, 0.95)' }}
-                    height="50"
-                    width="50"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"></path>
-                    <path d="M10 8v3a1 1 0 0 0 1 1h3"></path>
-                    <path d="M14 8v8"></path>
-                  </svg>
-                  <div className="space-y-2">
-                    <h5 className="text-xl font-semibold text-white transition group-hover:text-secondary">
-                      Policy Studies/ Development Research.
-                    </h5>
-                    <p className="text-gray-300">
-                      We love Celebrations. We will celebrate your launch on our
-                      Social Profiles.
-                    </p>
-                  </div>
-                </div>
-              </div>
+          </div>
+          <div className="flex">
+            <span className="flex-shrink-0 inline-flex justify-center items-center size-[46px] rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm mx-auto">
+              <DesktopComputerIcon className="flex-shrink-0 size-5" />
+            </span>
+            <div className="ms-5 sm:ms-8">
+              <h3 className="text-base sm:text-lg font-semibold text-gray-800">
+                ICT Training
+              </h3>
+              <p className="mt-1 text-gray-600">
+                Our ICT training includes computer-based test designs and administration to enhance digital literacy and skills.
+              </p>
             </div>
-            <div className="justify-center relative my-4 align-middle flex">
-              <a href="/services" className="btn bg-red-600 rounded-full hover:bg-red-500 hover:animate-pulse px-3 py-3 text-center text-white font-semibold">Learn More</a>
-
+          </div>
+          <div className="flex">
+            <span className="flex-shrink-0 inline-flex justify-center items-center size-[46px] rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm mx-auto">
+              <DocumentTextIcon className="flex-shrink-0 size-5" />
+            </span>
+            <div className="ms-5 sm:ms-8">
+              <h3 className="text-base sm:text-lg font-semibold text-gray-800">
+                Policy Research
+              </h3>
+              <p className="mt-1 text-gray-600">
+                We conduct policy research to provide insights and recommendations for informed decision-making.
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <span className="flex-shrink-0 inline-flex justify-center items-center size-[46px] rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm mx-auto">
+              <TrendingUpIcon className="flex-shrink-0 size-5" />
+            </span>
+            <div className="ms-5 sm:ms-8">
+              <h3 className="text-base sm:text-lg font-semibold text-gray-800">
+                Business Development Services
+              </h3>
+              <p className="mt-1 text-gray-600">
+                Our services include strategic business development, project management, and performance consulting.
+              </p>
             </div>
           </div>
         </div>
